06740074d1d8f5f7dc404bfa0410103304e5a7cb
shira_p <shiraparker@gmail.com>
2023-10-29 09:42:06 +0200

    added function that prints volume increase/decrease

--- volume_calculation/lession_volume_changes.py
+++ volume_calculation/lession_volume_changes.py
@@ -2,8 +2,8 @@
 from volume_calculation import generate_longitudinal_volumes_array
 
 
-def generate_volume_list_single_lesion():
-    longitudinal_volumes_array = generate_longitudinal_volumes_array()  # returns sorted (by date) array of
+def generate_volume_list_single_lesion(patient_path):
+    longitudinal_volumes_array = generate_longitudinal_volumes_array(patient_path)  # returns sorted (by date) array of
     # dictionaries (one for each time stamp), key - lesion idx, value - volume in mm^3
 
     print(longitudinal_volumes_array)
@@ -23,17 +23,32 @@ def generate_volume_list_single_lesion():
     return sorted_grouped_volumes
 
 
-print(generate_volume_list_single_lesion())
-
 
 """
-this fucntion gets a list of a lession's volume changes over time 
+this function gets a list of a lession's volume changes over time 
 and checks if the volume increased/decreased
 """
-def check_single_lession_growth(lession_volumes):
+def check_single_lession_growth(vol_list,lession_idx):
+    lession_volumes = vol_list[lession_idx]
     if not lession_volumes:
         print("No volume data available for the lesion.")
         return
-    
+    increasing = decreasing = True
 
+    for i in range(1, len(lession_volumes)-1):
+        if lession_volumes[i] > lession_volumes[i - 1]:
+            decreasing = False
+        elif lession_volumes[i] < lession_volumes[i - 1]:
+            increasing = False
+
+    if increasing:
+        print(f"Volumes are consistently increasing over time.")
+    elif decreasing:
+        print(f"Volumes are consistently decreasing over time.")
+    else:
+        print(f"Volumes show both increases and decreases over time.")
+
+vol_list = generate_volume_list_single_lesion("/cs/casmip/bennydv/liver_pipeline/gt_data/size_filtered/labeled_no_reg/A_W_")
+print(vol_list)
+check_single_lession_growth(vol_list,2)
 
