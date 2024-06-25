1972eeaf315301bcccc832c20dd29ec58fb4c00b
shira_p <shiraparker@gmail.com>
2024-06-25 15:38:47 +0300

    text for split and complex

--- text_generation/gen_text.py
+++ text_generation/gen_text.py
@@ -9,87 +9,7 @@ from generate_info.gen_single_lesion.drawer_single_lesion_graph import get_node_
 from reportlab.lib.styles import getSampleStyleSheet
 
 
-# nltk.download('wordnet')
 
-def check_lesion_growth_from_last_scan(lesion_volumes):
-    """
-    this function gets a list of a lesion's volume changes over time and
-    checks the difference from last 2 consecutive scans
-    :param lesion_volumes:
-    :return:
-    """
-    text = ""
-    if not lesion_volumes:
-        return "No volume data available for the lesion."
-    cur_volume = lesion_volumes[-1]
-    if len(lesion_volumes) < 2:
-        return "No volume data available for the lesion from previous scan. "
-
-    prev_volume = lesion_volumes[-2]
-    vol_change_type = ""
-    change_percentage = round(abs((cur_volume / prev_volume) - 1) * 100)
-    if cur_volume < prev_volume:
-        #The volume has increased by {change_percentage}% between the previous scan and the current scan.
-        text += f"The lesion volume has increased by {change_percentage}% from previous scan to current scan. "
-    elif cur_volume > prev_volume:
-        text += f"Lesion volume has decreased by {change_percentage}% from previous scan to current scan. "
-    else:
-        text += "No change in lesion volume from previous scan to current scan. "
-
-    return text
-
-def check_single_lesion_growth(lesion_volumes):
-    """
-    this function gets a list of a lesion's volume changes over time
-    and checks if the volume increased/decreased
-    """
-    
-    if not lesion_volumes:
-        return "No volume data available for the lesion. "
-
-    increasing = decreasing = True
-
-    text = check_lesion_growth_from_last_scan(lesion_volumes)
-
-    for i in range(1, len(lesion_volumes) - 1):
-        if lesion_volumes[i] > lesion_volumes[i - 1]:
-            decreasing = False
-        elif lesion_volumes[i] < lesion_volumes[i - 1]:
-            increasing = False
-    change_percentage = round(abs((lesion_volumes[-1] / lesion_volumes[0]) - 1) * 100)
-    if increasing:
-        text += f"The total lesion burden has monotonically increased, from first scan to last scan, by {change_percentage}%. "
-    elif decreasing:
-        text += f"The total lesion burden has monotonically decreased, from first scan to last scan, by {change_percentage}%. "
-    else:
-        text += f"Volume shows both increases and decreases over time from first scan to last scan. "
-    return text
-
-
-"""
-this function adds text describing percentage of volume growth for each time stamp
-"""
-
-def lesion_growth_percentage(longitudinal_volumes_array, num_of_tumors):
-    paragraph = []
-    volumes_array = get_diff_in_total(longitudinal_volumes_array)  # [total_vol_cm3, vol_percentage_diff, vol_cm3_diff]
-    if len(volumes_array) < 2:
-        return "Insufficient data for calculating percentage change."
-
-    # Extract the first and last elements
-    initial_value, _, _ = volumes_array[0]
-    final_value, vol_percentage_diff, vol_cm3_diff = volumes_array[-1]
-
-    # Determine the change type based on the sign of vol_percentage_diff
-    change_type = "increased" if vol_percentage_diff >= 0 else "decreased"
-    vol_percentage_diff_abs = round(abs((final_value / initial_value) - 1) * 100)
-    # Format the message based on the presence of a minus sign
-    vol_diff_text = f"{vol_percentage_diff_abs}%"
-
-    text = f"Tumor burden of {num_of_tumors} lesions {change_type} over time by {vol_diff_text}\n"
-    paragraph.append(Paragraph(text))
-
-    return paragraph
 
 
 def get_last_t_node(cur_componenet):
@@ -112,7 +32,8 @@ def get_vol_change_percentage(src,dest,vol_array):
 
     if src_vol != 0:
         percentage_diff = ((dest_vol/src_vol) - 1) * 100
-
+    if percentage_diff==100:
+        return 99
     return percentage_diff
 
 def join_with_and(strings):
@@ -135,9 +56,19 @@ def get_prev_appeared_nodes(node,edges_to_node_dict,vol_array):
             prev_nodes += get_prev_appeared_nodes(prev,edges_to_node_dict,vol_array)
     return prev_nodes
 
-def find_when_last_appeared_text(node,edges_to_node,vol_array):
+def find_when_last_appeared_text(node,edges_to_node_dict,vol_array,all_patient_dates,last_node):
     text =""
     prev_nodes = get_prev_appeared_nodes(node,edges_to_node_dict,vol_array)
+    if len(prev_nodes)>0:
+        time = prev_nodes[0].split("_")[1]
+        date = all_patient_dates[int(time)]
+        text+=f"It last appeared in the scan taken on {date}. "
+        percentage = get_vol_change_percentage(prev_nodes[0],last_node,vol_array) # only works for single prev node
+        if percentage<0 :
+            changed ="decreased"
+        else:
+            changed="increased"
+        text +=f"This lesion’s volume has {changed} by {abs(int(percentage))}% between the previous scan where it last appeared ({date}) and the current scan. "
     
 
 
@@ -146,29 +77,48 @@ def doesnt_appear_or_lone_lesion(ld,last_node,pattern,vol_array,all_patient_date
     edges_to_node_dict = get_edges_to_node_dict(ld)
     last_seen,_ = edges_to_node_dict[last_node][0]
     last_seen_time = last_seen.split("_")[1]
-    cur_time = last_node.split("_")[1]
-    cur_node_idx=last_node.split("_")[0]
+    last_node_time = last_node.split("_")[1]
+    last_node_idx=last_node.split("_")[0]
     text =""
+    doesnt_appear_nodes_times = {int(node.split("_")[1]) for node in doesnt_appear_nodes.keys()}
     if pattern =="lone":
-        text+= f"This is a lone lesion. It appeared in the scan taken on {all_patient_dates[int(cur_time)]}. "
-        if len(vol_array)==int(cur_time)-1:
-            if int(last_node.split("_")[0]) in vol_array[int(cur_time)-1]:
-                cur_vol = vol_array[int(cur_time)-1][int(last_node.split("_")[0])]
+        text+= f"This is a lone lesion. It appeared in the scan taken on {all_patient_dates[int(last_node_time)]}. "
+        if len(vol_array)==int(last_node_time)-1:
+            if int(last_node.split("_")[0]) in vol_array[int(last_node_time)-1]:
+                cur_vol = vol_array[int(last_node_time)-1][int(last_node.split("_")[0])]
                 text+= f"This lesion's volume is {cur_vol} cc. "
                 return text
-    date = "date"
-    if int(cur_node_idx) in vol_array[int(cur_time)]:
-        cur_node_vol = vol_array[int(cur_time)][int(cur_node_idx)]
-        text+=f"This lesion doesn't appear in the previous scan, taken on {all_patient_dates[int(last_seen_time)]}. "
-        # text += find_when_last_appeared_text()
-        text+=f"The currrent lesion volume is {round(cur_node_vol,2)} cc. "
-        text+= get_volume_diff_from_first_scan(ld, last_node,cur_component,vol_array)
+    if last_node != next(iter(doesnt_appear_nodes.keys())):
+        if len(doesnt_appear_nodes_times)>1:
+
+            text += "This lesion doesn't appear in the previous scans, taken on " + ", ".join(
+            [all_patient_dates[int(last_seen_time)] for last_seen_time in doesnt_appear_nodes_times]) + ". "
+        else:
+            text+=f"This lesion doesn't appear in the previous scan, taken on {all_patient_dates[int(next(iter(doesnt_appear_nodes_times)))]}. "
+            text += find_when_last_appeared_text(next(iter(doesnt_appear_nodes.keys())),edges_to_node_dict,vol_array,all_patient_dates,last_node)
+        if int(last_node_idx) in vol_array[int(last_node_time)]:
+            cur_node_vol = vol_array[int(last_node_time)][int(last_node_idx)]
+            if len(last_node)==1:
+                text+=f"The currrent lesion volume is {round(cur_node_vol,2)} cc. "
+        # text+= get_volume_diff_from_first_scan(ld, last_node,cur_component,vol_array)
+        return text
+    if int(last_node_idx) in vol_array[int(last_node_time)]:
+        cur_node_vol = vol_array[int(last_node_time)][int(last_node_idx)]
+        if len(doesnt_appear_nodes_times)>1:
+
+            text += "This lesion doesn't appear in the previous scans, taken on " + ", ".join(
+            [all_patient_dates[int(last_seen_time)] for last_seen_time in doesnt_appear_nodes_times]) + "."
+        else:
+            text+=f"This lesion doesn't appear in the previous scan, taken on {all_patient_dates[int(last_seen_time)]}. "
+        text += find_when_last_appeared_text(next(iter(doesnt_appear_nodes.keys())),edges_to_node_dict,vol_array,all_patient_dates,last_node)
+        if len(last_node)==1:
+            text+=f"The currrent lesion volume is {round(cur_node_vol,2)} cc. "
         return text
     
 
-    text += f"The lesion does not appear in the scan taken on {all_patient_dates[int(cur_time)]}."
+    text += f"The lesion does not appear in the scan taken on {all_patient_dates[int(last_node_time)]}. "
     if last_seen:
-        text+=f"It last appeared in the scan taken on {all_patient_dates[int(last_seen_time)]}"
+        text+=f"It last appeared in the scan taken on {all_patient_dates[int(last_seen_time)]}. "
 
     return text
 
@@ -250,7 +200,7 @@ def get_volume_diff_from_first_scan(ld,cur_node,cur_component,vol_array):
             percentage = abs(math.floor(((cur_vol/first_node_vol) - 1) * 100))
 
     if not first_node_vol:
-        return "There is no volume data for this lesion from first scan"
+        return "There is no volume data for this lesion from the first scan. "
    
     change = "decreased"
     if first_node_vol< cur_vol:
@@ -260,24 +210,97 @@ def get_volume_diff_from_first_scan(ld,cur_node,cur_component,vol_array):
     monotonic_flag = is_monotonic(cur_vol,prev_nodes,vol_array,cur_component)
     if monotonic_flag:
         monotonic_text = "monotonically"
-    
+    if percentage ==100:
+        percentage=99
     text =f"The total lesion burden has {monotonic_text} {change}, from the first scan to the current scan, by {percentage}%. "
     return text
 
 
-def gen_text_single_node(ld,last_node,nodes2cc_class,edge_vol_change_class,edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict,doesnt_appear_nodes=[]):
-    text =""
-    # check pattern type of cc
-    pattern = nodes2cc_class[last_node]
-    doesnt_appear_cc_dict = {node:nodes2cc_class[node] for node in doesnt_appear_nodes}
-    vol_change_percentage,edge_class = edge_vol_change_class
-    vol_change_percentage = abs(round(vol_change_percentage))
-    doesnt_appear_flag =False
-    if edge_class == "empty":
-        doesnt_appear_flag=True
-        text = doesnt_appear_or_lone_lesion(ld,last_node,pattern,longitudinal_volumes_array,all_patient_dates,cur_component,doesnt_appear_cc_dict)
-        return text
+def find_linear_node(last_nodes,edges_list,volume_change_per_edge_dict):
+    # for edge in iter(edges_list.values()):
+    #     if volume_change_per_edge_dict[edge[0]][1]!="splitting":
+    #         linear_node = edge[0]
+    #         return linear_node
+    # return last_nodes[0]
 
+    def trace_back_pre_split(lesion, timestamp):
+        for (src, tgt) in volume_change_per_edge_dict.keys():
+            src_lesion, src_time = src.split('_')
+            tgt_lesion, tgt_time = tgt.split('_')
+            
+            if int(tgt_lesion) == lesion and int(tgt_time) == timestamp:
+                # Check if the source lesion has multiple outgoing edges
+                outgoing_edges = [(s, t) for (s, t) in volume_change_per_edge_dict.keys() if s == src]
+                if len(outgoing_edges) > 1:
+                    # This is the split point, return the lesion just before split
+                    return src
+                else:
+                    # Continue tracing back
+                    return trace_back_pre_split(int(src_lesion), int(src_time))
+        # If no predecessor is found, return this lesion (origin lesion)
+        return f"{lesion}_{timestamp}"
+
+    pre_split_lesions = {}
+
+    # For each lesion in the last timestamp, trace back to the lesion just before the split
+    for lesion in last_nodes:
+        lesion_id, timestamp = map(int, lesion.split('_'))
+        pre_split_lesion = trace_back_pre_split(lesion_id, timestamp)
+        pre_split_lesions[lesion] = pre_split_lesion
+
+    return pre_split_lesions[last_nodes[0]]
+
+def get_text_splitting(last_nodes,vol_change_percentage,longitudinal_volumes_array,edges_list,volume_change_per_edge_dict):
+    text =""
+    linear_node = find_linear_node(last_nodes,edges_list,volume_change_per_edge_dict)
+    joined_nodes =join_with_and(last_nodes)
+    text+=f"The lesion {linear_node} split into {len(last_nodes)} lesions- {joined_nodes} . "
+
+    # find total currrent volume
+    total_cur_vol =0
+    for node in last_nodes:
+        i,time = node.split("_")
+        i=int(i)
+        time = int(time)
+        if time <= len(longitudinal_volumes_array)-1:
+            if i in longitudinal_volumes_array[time]:
+                total_cur_vol+=longitudinal_volumes_array[time][i]
+    text+=f"The current lesion burden is {round(total_cur_vol,2)} cc. "
+    total_vol_first_scan = 0
+    for vol in longitudinal_volumes_array[0]:
+        total_vol_first_scan+= vol
+    change = "decreased"
+    if vol_change_percentage>0:
+        change = "increased"
+    text+=f"The current tumor burden has {change} by {abs(round(vol_change_percentage))}% since the previous scan "
+        
+
+
+    # find difference from first scan
+
+
+
+    return text
+
+
+def gen_text_single_node(ld,last_node,nodes2cc_class,edge_vol_change_class,edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict,doesnt_appear_nodes={}):
+    text =""
+    if len(last_node)<1:
+        return text
+    last_node_val = last_node[0]
+    # check pattern type of cc
+    pattern = nodes2cc_class[last_node_val]
+    doesnt_appear_cc_dict = {node:nodes2cc_class[node] for node in doesnt_appear_nodes}
+    vol_change_percentage = 0
+    if len(edges_list[last_node_val])>0:
+        vol_change_percentage,edge_class = edge_vol_change_class[edges_list[last_node_val][0]]
+    # vol_change_percentage = abs(round(vol_change_percentage))
+    # doesnt_appear_flag =False
+    if len(doesnt_appear_nodes)>0:
+        doesnt_appear_flag=True
+        text += doesnt_appear_or_lone_lesion(ld,last_node_val,pattern,longitudinal_volumes_array,all_patient_dates,cur_component,doesnt_appear_cc_dict)
+        # return text
+    change =""
     if vol_change_percentage<0:
         change = "decreased"
         
@@ -285,8 +308,7 @@ def gen_text_single_node(ld,last_node,nodes2cc_class,edge_vol_change_class,edges
         change ="increased"
 
     if pattern =='merge_p':
-        # connected_nodes =[]
-        connected_nodes = [node1 for node1, node2 in edges_list]
+        connected_nodes = [node1 for node1, node2 in edges_list[last_node_val]]
         prev_lesions_for_text = connected_nodes
         lesion_text = "relative to the combined (summed) previous volumes of lesions"
         prev_lesions_for_text_list =join_with_and(prev_lesions_for_text)
@@ -303,26 +325,28 @@ def gen_text_single_node(ld,last_node,nodes2cc_class,edge_vol_change_class,edges
                 connected_nodes.remove(prev_node)
         connected_nodes_list = join_with_and(connected_nodes)
         
-        text +=f"Lesion {last_node} is a merged lesion resulting from a merge of lesions {connected_nodes_list}. "
-        if not doesnt_appear_flag:
-            text += f"Lesion {last_node}'s total volume, {lesion_text} {prev_lesions_for_text_list}, has {change} by {vol_change_percentage}%. "
-            text+= get_volume_diff_from_first_scan(ld, last_node,cur_component,longitudinal_volumes_array)
+        text +=f"Lesion {last_node_val} is a merged lesion resulting from a merge of lesions {connected_nodes_list}. "
+        if vol_change_percentage!=0:
+            text += f"Lesion {last_node_val}'s total volume, {lesion_text} {prev_lesions_for_text_list}, has {change} by {abs(round(vol_change_percentage))}%. "
+            text+= get_volume_diff_from_first_scan(ld, last_node_val,cur_component,longitudinal_volumes_array)
         return text
     
     ## todo
     if pattern == 'split_p':
-        pass
+        text+=f"The lesion is a result of a split of the previous lesion into multiple lesions. "
 
     if pattern == 'complex_p':
-        pass
+        text+= f"The lesion classification is complex. "
+        if edge_class =="splitting":
+            # find total tumor burden in current scan
+            text += get_text_splitting(last_node,vol_change_percentage,longitudinal_volumes_array,edges_list,volume_change_per_edge_dict)
+            # compare to total tumor burden in first scan and return text for volume change since first scan
+
+            return text
     
-    # check if total vol is increase/decrease and monotonic
-    # percentage_diff = get_vol_change_percentage(prev_node,last_node)
-
-    # if vol_change_percentage>0:
-    text += f"The current lesion volume has {change} over time by {vol_change_percentage}% relative to the previous scan. "
-    text+= get_volume_diff_from_first_scan(ld, last_node,cur_component,longitudinal_volumes_array)
-
+    if change!="":
+        text += f"The current lesion volume has {change} over time by {abs(round(vol_change_percentage))}% relative to the previous scan. "
+    text+= get_volume_diff_from_first_scan(ld, last_node_val,cur_component,longitudinal_volumes_array)
 
 
     return text
@@ -346,20 +370,19 @@ def gen_summary_for_cc(ld,cur_component,longitudinal_volumes_array,nodes2cc_clas
     for node in last_nodes:
         matching_keys = [key for key in volume_change_per_edge_dict.keys() if node in key]
         edges_dict[node]=matching_keys
-    if len(last_nodes)==1:
-        last_node_par=last_nodes[0]
-        edges_list =edges_dict[last_node_par]
+    # if len(last_nodes)==1:
+    #     last_node_par=last_nodes[0]
+    #     edges_list =edges_dict[last_node_par]
 
-        # check if there are no edges from this node. meaning node either doesnt appear or is lone
-        if len(doesnt_appear_nodes)>0:
-            text+= gen_text_single_node(ld,last_node_par,nodes2cc_class,(0,"empty"),edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict,doesnt_appear_nodes)
+        # if len(doesnt_appear_nodes)>0:
+        #     text+= gen_text_single_node(ld,last_node_par,nodes2cc_class,(0,"empty"),edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict,doesnt_appear_nodes)
 
-        else:
-            input_par =volume_change_per_edge_dict[edges_list[0]]
-            text += gen_text_single_node(ld,last_node_par,nodes2cc_class,input_par,edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict)
+        # else:
+        #     input_par =volume_change_per_edge_dict[edges_list[0]]
+        #     text += gen_text_single_node(ld,last_node_par,nodes2cc_class,input_par,edges_list,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict)
+    
+    text += gen_text_single_node(ld,last_nodes,nodes2cc_class,volume_change_per_edge_dict,edges_dict,longitudinal_volumes_array,all_patient_dates,cur_component,volume_change_per_edge_dict,doesnt_appear_nodes)
 
-    ## need to support case of several last nodes in cc
-    # check_single_lesion_growth(vol_list)
     text2 = replace_lesion_names(text,internal_external_names_dict)
     paragraph_text =Paragraph(text2,getSampleStyleSheet()['Normal'])
 
